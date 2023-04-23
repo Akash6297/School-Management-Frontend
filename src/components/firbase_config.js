9f8f3a4ce42821d4687968dd46842bb2d41e2523
Akash Mandal <akashmandal6297@gmail.com>
2023-04-23 13:51:36 +0530

    first commit

---
+++ src/components/firbase_config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC60z7atajL2fHNQOFJA81UiO5tnHqT5BQ",
+  authDomain: "verify-7f545.firebaseapp.com",
+  projectId: "verify-7f545",
+  storageBucket: "verify-7f545.appspot.com",
+  messagingSenderId: "476271760221",
+  appId: "1:476271760221:web:83ac1b2db6bf1dbfafceb4",
+  measurementId: "G-FX5T00JWPE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/components/reset.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react'
+
+export default class Reset extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+        email: "",
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleSubmit(e){
+        e.preventDefault();
+        const { email } = this.state;
+        console.log(email );
+        fetch("http://localhost:5000/forgot-password", {
+            method:"POST",
+            crossDomain:true,
+            headers:{
+              "Content-Type":"application/json",
+              Accept:"application/json",
+              "Access-Control-Allow-Origin":"*",
+            },
+            body:JSON.stringify({
+              email,
+              
+            }),
+        }).then((res)=>res.json())
+    .then((data)=>{
+      console.log(data, "userRegister");
+      alert(data.status);
+      
+    });
+    
+    }
+
+    render(){
+        return (
+            <div className="auth-wrapper">
+      <div className="auth-inner">
+            <form onSubmit={this.handleSubmit}>
+                <h3>Forget Password</h3>
+                
+                <div className='mb-3'>
+                    <label>Email address</label>
+                    <br />
+                    <input
+                    type="email"
+                    className='from-control'
+                    
+                    placeholder='Enter email'
+                    onChange={(e) => this.setState({email: e.target.value})}
+                    />
+                </div>
+
+                <div className='d-grid'>
+                    <button type='submit' className="btn btn-primary">
+                        Submit
+                    </button>
+                </div>
+                <p className='forgot-password text-right'>
+                    <a href='/sign-up'>Sign Up</a>
+                </p>
+
+            </form>
+            </div>
+    </div>
+        )
+    }
+}
